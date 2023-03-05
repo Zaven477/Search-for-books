193acd56ef5d0e919597f844215547c6a2c16394
Zaven477 <papikyan86@bk.ru>
2023-03-05 18:40:27 +0400

    init app

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { Books } from './components/Books';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BooksById } from './components/BooksById';
 
-function App() {
+
+
+
+const App = () => {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={ <Books /> } />
+        <Route path="books/:id" element={ <BooksById /> } />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
