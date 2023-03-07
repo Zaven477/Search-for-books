c29061df1a8146723af1a97861e44223b62b69f9
Zaven477 <papikyan86@bk.ru>
2023-03-07 21:06:59 +0400

    book sorting

---
+++ src/components/BookSorting.tsx
@@ -0,0 +1,28 @@
+import { useAppDispatch } from "../hooks"
+import { resetBooks } from "../store/booksSlice";
+import { getBooks } from "../store/effects";
+
+interface ISortingProps {
+    value: string;
+    
+}
+
+export const BookSorting = ({ value }: ISortingProps) => {
+  const dispatch = useAppDispatch();
+
+    const sortBooks = (option: string) => {
+        dispatch(resetBooks([]));
+        dispatch(getBooks({ value: value, page: 0, orderBy: option }))
+        
+    }
+  
+
+    return (
+        <div className="container">
+           <select className="sortForm" onChange={ (event) => sortBooks(event.target.value) }>
+             <option value="relevance">relevance</option>
+             <option value="newest">newest</option>
+           </select>
+         </div>
+    )
+}
