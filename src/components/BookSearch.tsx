d9dcbd5abacaa2e41ef4d5f0988c926661d8c498
Zaven477 <papikyan86@bk.ru>
2023-03-07 18:20:01 +0400

    update book search

--- src/components/BookSearch.tsx
+++ src/components/BookSearch.tsx
@@ -1,41 +1,40 @@
-import { FormEvent, useState } from "react";
+import { FormEvent } from "react";
 import { resetBooks } from "../store/booksSlice";
 import { getBooks } from "../store/effects";
-import { useAppDispatch } from '../hooks';
+import { useAppDispatch } from "../hooks";
 
+interface IValueProps {
+  value: string;
+  setValue: React.Dispatch<React.SetStateAction<string>>;
+}
 
+export const BookSearch = ({ value, setValue }: IValueProps) => {
+  const dispatch = useAppDispatch();
 
-export const BookSearch = () => {
-    const [value, setValue] = useState('');
-    const dispatch = useAppDispatch();   
+  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
 
-    
-    
-    const handleSubmit = async(event: FormEvent<HTMLFormElement>) => {
-        event.preventDefault();
-     
-        if(value.trim().length === 0) {
-           return;
-        }
-        
-        dispatch(resetBooks([]));
-        dispatch(getBooks({ value: value, page: 0 }))
-        setValue('');
-
+    if (value.trim().length === 0) {
+      return;
     }
 
-    return (
+    dispatch(resetBooks([]));
+    dispatch(getBooks({ value: value, page: 0 }));
+  };
+
+  return (
     <div>
-        <h1>Search for books</h1>
-         <form onSubmit={ handleSubmit }>
-            <input type="text" 
-                   name="name" 
-                   className="width" 
-                   onChange={ (event) => setValue(event.target.value) }
-                   value={value}
-                   />
-            <input type="submit" value="Search" className="btn"/>
-         </form>
+      <h1>Search for books</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          className="width"
+          onChange={(event) => setValue(event.target.value)}
+          value={value}
+        />
+        <input type="submit" value="Search" className="btn" />
+      </form>
     </div>
-    )
-}
+  );
+};
