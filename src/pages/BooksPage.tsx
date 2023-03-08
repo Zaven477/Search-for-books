29983179fddd677a2ed68fa04b3877eaa9c838e4
Zaven477 <papikyan86@bk.ru>
2023-03-08 18:04:24 +0400

    books page correction

--- src/pages/BooksPage.tsx
+++ src/pages/BooksPage.tsx
@@ -6,19 +6,17 @@ import { FilterByCategory } from "../components/FilterByCategory";
 import { Pagination } from "../components/Pagination";
 import { useAppSelector } from "../hooks";
 
-
 export const BooksPage = () => {
   const [value, setValue] = useState("");
-  const [filter, setFilter] = useState('');
+  const [filter, setFilter] = useState("");
   const books = useAppSelector((state) => state.books.items);
   const totalBooks = useAppSelector((state) => state.books.totalItems);
-  
-  
+
   return (
     <div>
       <div className="form">
         <BookSearch value={value} setValue={setValue} />
-        <FilterByCategory setFilter={setFilter}/>
+        <FilterByCategory setFilter={setFilter} />
         <BookSorting value={value} />
       </div>
       <BookList books={books} totalBooks={totalBooks} />
