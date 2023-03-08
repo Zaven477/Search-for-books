7287b5beedf30893e31eafe407bdf2209ec9ab17
Zaven477 <papikyan86@bk.ru>
2023-03-08 18:02:54 +0400

    pagination adjustment

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -6,29 +6,29 @@ import { getBooks } from "../store/effects";
 interface IValue {
   value: string;
   filter: string;
-  
 }
 
 export const Pagination = ({ value, filter }: IValue) => {
-    const loadingBooks = useAppSelector((state) => state.books.loading);
-    const [indexPagination, setIndexPagination] = useState(startIndexPagination);
-    const nextPage = useAppSelector((state) => state.books.hasNextPage);
-    const dispatch = useAppDispatch();
+  const loadingBooks = useAppSelector((state) => state.books.loading);
+  const [indexPagination, setIndexPagination] = useState(startIndexPagination);
+  const nextPage = useAppSelector((state) => state.books.hasNextPage);
+  const dispatch = useAppDispatch();
 
-    const valueOrCategory = value ? value : filter;
-    
+  const valueOrCategory = value ? value : filter;
 
-    const pagination = () => {
-        dispatch(getBooks({ value: `${valueOrCategory}`, page: indexPagination }));
-        setIndexPagination(indexPagination + 30);
-      };
-    
-    return (
-       <div>
-        { loadingBooks && <p className="text-center">Loading...</p> }
-        { nextPage && <button className="btn-more" onClick={pagination} disabled={!nextPage}>
-            Load more...
-         </button> }
-      </div>
-    )
-}
+  const pagination = () => {
+    dispatch(getBooks({ value: `${valueOrCategory}`, page: indexPagination }));
+    setIndexPagination(indexPagination + 30);
+  };
+
+  return (
+    <div>
+      {loadingBooks && <p className="text-center">Loading...</p>}
+      {nextPage && (
+        <button className="btn-more" onClick={pagination} disabled={!nextPage}>
+          Load more...
+        </button>
+      )}
+    </div>
+  );
+};
