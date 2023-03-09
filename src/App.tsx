9caf595d350d9ad9a7cfe2a9d90d2ecdf2a55cd2
Zaven477 <papikyan86@bk.ru>
2023-03-09 22:43:19 +0400

    adding dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,18 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { BooksPage } from './pages/BooksPage';
-
-
-
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BooksPage } from "./pages/BooksPage";
+import { BookPage } from "./pages/BookPage";
 
 const App = () => {
-  
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={ <BooksPage /> } />
-        {/* <Route path="books/:id" element={ <BooksById /> } /> */}
+        <Route path="/" element={<BooksPage />} />
+        <Route path="books/:id" element={<BookPage />} />
       </Routes>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
---
+++ src/pages/BookPage.tsx
@@ -0,0 +1,41 @@
+import { useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { useAppDispatch, useAppSelector } from "../hooks";
+import { getBook } from "../store/effects";
+import parse from 'html-react-parser';
+import { IBooks } from "../store/types";
+
+export const BookPage = () => {
+  const params = useParams<{ id: string }>();
+  const id = params.id || "";
+  const book: IBooks | undefined = useAppSelector((state) => state.book.book);
+  const loading = useAppSelector((state) => state.book.loading);
+  const error = useAppSelector((state) => state.book.errors);
+  
+
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getBook(id));
+  }, [id, dispatch]);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p className="text-error">{error}</p>;
+  }
+
+  if (!book) return null;
+
+  return (
+    <div>
+      <img src={book.volumeInfo?.imageLinks?.smallThumbnail} alt="" />
+      <p>{book.volumeInfo?.title}</p>
+      <p>{book.volumeInfo?.categories}</p>
+      <p>{book.volumeInfo?.authors}</p>
+      <p>{parse(book.volumeInfo?.description)}</p>
+    </div>
+  );
+};
