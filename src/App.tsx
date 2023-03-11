69369d91e8a84558872804cf4ed992bd08464ff8
Zaven477 <papikyan86@bk.ru>
2023-03-11 17:17:26 +0400

    creating a non-existent page and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { BooksPage } from "./pages/BooksPage";
-import { BookPage } from "./pages/BookPage";
+import { BooksPage } from "./pages/BooksPage/BooksPage";
+import { BookPage } from "./pages/BookPage/BookPage";
+import { NotFoundPage } from "./components/NotFoundPage/NotFoundPage";
+
 
 const App = () => {
   return (
@@ -10,6 +12,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<BooksPage />} />
         <Route path="books/:id" element={<BookPage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/NotFoundPage/NotFoundPage.tsx
@@ -0,0 +1,10 @@
+import "./style.css";
+
+export const NotFoundPage = () => {
+    return (
+        <div>
+          <p className="content">No content</p>
+          <a href="/" className="back">Go back</a>
+        </div>
+    )
+}
